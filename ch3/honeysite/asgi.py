9cf9cf4c3af9609e343fc692879c0fc73650f65c
Spstars <gijin100@naver.com>
2021-06-27 14:31:34 +0900

    vote하는 웹 작성

---
+++ ch3/honeysite/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for honeysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'honeysite.settings')
+
+application = get_asgi_application()
